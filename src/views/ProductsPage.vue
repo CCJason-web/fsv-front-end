52fa635e00621bf47c54cd839a7686493bcac117
pkjin1013 <pkjin1013@126.com>
2020-12-19 14:05:18 +0800

    Branch to master

--- src/views/ProductsPage.vue
+++ src/views/ProductsPage.vue
@@ -9,7 +9,7 @@
 				<h3 class="product-name">{{ product.name }}</h3>
 				<p class="product-price">${{ product.price }}</p>
 				<router-link v-bind:to="'/products/' + product.id">
-					<button>View Details, Haha</button>
+					<button>View Details</button>
 				</router-link>
 			</div>
 		</div>
